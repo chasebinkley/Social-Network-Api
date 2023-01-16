8ce0adae9ee4543252bfb55ed98bbdc0ae85251a
Chase Binkley <chasebinkley@Chases-MBP.lan>
2023-01-16 11:55:51 -1000

    adds walkthrough video

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Social-Network-Api
+
+## Description
+
+
+
+##### Walkthrough Video
+
+[Link to walkthrough video](./assets/walkthrough.webm)
--- server.js
+++ server.js
@@ -9,18 +9,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(require('./routes'));
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
-//   // useFindAndModify: true,
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// });
-
-// mongoose.set('debug', true);
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     
   });
 });
-
-// app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
